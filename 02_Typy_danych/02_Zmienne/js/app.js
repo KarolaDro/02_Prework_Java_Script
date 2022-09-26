1efb6fd532b1eb6cc8fd69d9fa34e60d1261904d
Karolina Droździel <k_drozdziel@o2.pl>
2022-09-26 23:03:41 +0200

    Zmienne

---
+++ 02_Typy_danych/02_Zmienne/js/app.js
@@ -0,0 +1,26 @@
+// Ta zmienna jest typu number i przechowuje wartość 25
+const numberValue = 25;
+console.log(numberValue);
+console.log(typeof numberValue);
+
+// Ta zmienna jest typu string i przechowuje tekst
+const stringValue = "To jest mój string";
+console.log(stringValue);
+console.log(typeof stringValue);
+
+// Ta zmienna jest typu string i łączy wartość i tekst
+const number = 2;
+const text = "dwa";
+const mixedValue = number + " " + text;
+console.log(mixedValue);
+console.log(typeof mixedValue);
+
+// Ta zmienna jest typu boolean i przechowuje wartość logiczną
+const logicValue = true;
+console.log(logicValue);
+console.log(typeof logicValue);
+
+// Ta zmienna jest typu object i przechowuje pustą wartość
+const nullValue = null;
+console.log(nullValue);
+console.log(typeof nullValue);
