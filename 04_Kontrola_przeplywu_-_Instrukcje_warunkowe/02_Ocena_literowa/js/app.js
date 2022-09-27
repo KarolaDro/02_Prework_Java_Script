3b08fcd13915a5b8c90c42fbf0fb659ecdb24565
Karolina Droździel <k_drozdziel@o2.pl>
2022-09-27 22:30:49 +0200

    Ocena literowa

--- 04_Kontrola_przeplywu_-_Instrukcje_warunkowe/02_Ocena_literowa/js/app.js
+++ 04_Kontrola_przeplywu_-_Instrukcje_warunkowe/02_Ocena_literowa/js/app.js
@@ -1,1 +1,29 @@
 const grade = 4;
+
+switch (grade) {
+    case (1): {
+        console.log("F");
+        break;
+    }
+    case (2): {
+        console.log("E");
+        break;
+    }
+    case (3): {
+        console.log("D");
+        break;
+    }
+    case (4): {
+        console.log("C");
+        break;
+    }
+    case (5): {
+        console.log("B");
+        break;
+    }
+    case (6): {
+        console.log("A");
+        break;
+    }
+
+}
