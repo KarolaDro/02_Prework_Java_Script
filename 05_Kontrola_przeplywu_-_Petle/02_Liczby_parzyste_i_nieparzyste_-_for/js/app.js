1a7354bb8bff6010f2683ccb44fa5448be93a6c6
Karolina Droździel <k_drozdziel@o2.pl>
2022-09-28 13:55:22 +0200

    Liczby parzyste i nieparzyste

--- 05_Kontrola_przeplywu_-_Petle/02_Liczby_parzyste_i_nieparzyste_-_for/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/02_Liczby_parzyste_i_nieparzyste_-_for/js/app.js
@@ -4,3 +4,15 @@ const n = 13;
  * Write your code below!
  */
 
+for ( let i = 0; i <= n; i = i + 1 ) {
+    if ( i % 2 === 0 ) {
+        console.log(i + " - even");
+    } else {
+        console.log(i + " - odd");
+    }
+}
+
+
+
+
+
