b1fbfb9829ae055abe8305d5bf2961b71c195034
Karolina Droździel <k_drozdziel@o2.pl>
2022-09-29 14:15:24 +0200

    Tworzenie tablicy 2D

--- 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/06_Tworzenie_tablicy_2D/js/app.js
@@ -6,3 +6,16 @@ const rows = 5;
  * Write your code below!
  */
 
+let count = 1;
+for (let i = 0; i < rows; i++)
+{
+    numbers.push([]);
+
+    for (let j = 0; j < columns; j++)
+    {
+        numbers[i].push(count);
+        count++;
+    }
+}
+console.log(numbers);
+
