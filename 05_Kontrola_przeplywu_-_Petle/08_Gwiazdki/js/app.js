b368f9a818a54b6182aeba104599a15cd24995b5
Karolina Droździel <k_drozdziel@o2.pl>
2022-09-29 23:51:10 +0200

    Gwiazdki

--- 05_Kontrola_przeplywu_-_Petle/08_Gwiazdki/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/08_Gwiazdki/js/app.js
@@ -4,3 +4,25 @@ const n = 5;
  * Write your code below!
  */
 
+const tab = [];
+for (let i = 0; i < n; i++) {
+        for (let j = i; j >= i; j--) {
+            tab.push("*");
+            console.log(tab);
+        }
+    }
+
+
+
+
+/**
+BEZ TABLICY:
+let star = "";
+for (let i=0; i<n; i++) {
+        for (let j = 0; j <= i; j++) {
+            star += "*";
+        }
+        star += "\n";
+    }
+console.log(star);
+ */
