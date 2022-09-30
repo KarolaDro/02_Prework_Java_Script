cf753f517ec1471bb73ed466a47bf804f57bdf5d
Karolina Droździel <k_drozdziel@o2.pl>
2022-09-30 12:24:59 +0200

    Tabliczka mnożenia

--- 05_Kontrola_przeplywu_-_Petle/07_Tabliczka_mnozenia/js/app.js
+++ 05_Kontrola_przeplywu_-_Petle/07_Tabliczka_mnozenia/js/app.js
@@ -3,5 +3,13 @@ const calc = [];
 
 /**
  * Write your code below!
- */
+  */
 
+for (let i = 0; i < n; i++) {
+    calc.push([]);
+    for (let j = 1; j <= n; j++) {
+        calc[i].push(i + " x " + j + " = " + i * j);
+    }
+    console.log(calc[i].join(" | "));
+}
+console.log(calc);
