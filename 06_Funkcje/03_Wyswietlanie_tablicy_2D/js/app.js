5d81201ab741d0a484e5c5299963041b1062a4e5
Karolina Droździel <k_drozdziel@o2.pl>
2022-10-01 23:37:43 +0200

    Wyświetlanie tablicy 2D

--- 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
+++ 06_Funkcje/03_Wyswietlanie_tablicy_2D/js/app.js
@@ -11,3 +11,12 @@ const users = [["Jaydn Humphries", "Ayda Orozco"], ["Sanjeev Wilkinson", "Jorge 
  * Do NOT modify code below!
  */
 module.exports = typeof print2DArray === "undefined" ? null : print2DArray;
+
+function print2DArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            console.log(array[i][j]);
+        }
+    }
+}
+print2DArray(users);
