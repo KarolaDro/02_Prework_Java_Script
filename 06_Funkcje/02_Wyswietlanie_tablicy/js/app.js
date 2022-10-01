07d3151fd54f4c73069532ac84d557ba26867a87
Karolina Droździel <k_drozdziel@o2.pl>
2022-10-01 22:21:26 +0200

    Wyświetlanie tablicy

--- 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
+++ 06_Funkcje/02_Wyswietlanie_tablicy/js/app.js
@@ -8,3 +8,10 @@ const people = ["John", "Ive", "Donna", "Chris"];
  * Do NOT modify code below!
  */
 module.exports = typeof printArray === "undefined" ? null : printArray;
+
+function printArray(array) {
+    for (let i = 0; i < array.length; i++) {
+console.log(array[i]);
+    }
+}
+printArray(people);
