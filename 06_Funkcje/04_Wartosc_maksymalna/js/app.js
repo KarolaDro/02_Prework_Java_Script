4def006fcc353f3d776ec50bd5e9e6221662ddfc
Karolina Droździel <k_drozdziel@o2.pl>
2022-10-02 21:27:00 +0200

    Wartość maksymalna

--- 06_Funkcje/04_Wartosc_maksymalna/js/app.js
+++ 06_Funkcje/04_Wartosc_maksymalna/js/app.js
@@ -3,3 +3,16 @@ const randomNumbers = [27, 64, 47, 78, 48, 11, 76, 25, 11, 83];
 /**
  * Write your code below!
  */
+
+function maxFromArray(numbers) {
+    let max = numbers[0];
+    for (let i = 0; i < randomNumbers.length; i++) {
+        if (randomNumbers[i] > max) {
+            max = randomNumbers[i];
+        }
+    }
+    return max;
+}
+const result = maxFromArray(randomNumbers);
+console.log(result);
+
